b02bc867709a6c7a532f517c2401d5effa54ecf3
Tobbyte <7722596+Tobbyte@users.noreply.github.com>
2026-07-20 15:45:29 +0200

    refactor main content

---
+++ base_app/base_app.py
@@ -0,0 +1,31 @@
+"""Main Brain of the Game.
+
+
+#show_start_screen() -> None
+    #show_menu() -> None
+    #wiki_response = call_wiki_api(thema:str)->str
+    #wiki_response_parsed = parse_api_response(wiki_response:str)->dict
+    #append_conversation_history()->None # as dictionary
+    #persona = construct_persona()->str
+    #llm_response = call_llm(wiki_response_parsed, persona) #Response ohne Bezug auf das Thema
+    #append_conversation_history()->None # as dictionary
+    #While-Loop bis der User richtig erraten oder sein Leben verbraucht hat
+        #user_input = take_user_response()->str
+        #append_conversation_history()->None # as dictionary
+        #response = call_llm(user_input)->str # response ist
+        #append_conversation_history()->None # as dictionary
+    #Endloop
+    #show_result(conversation_history)->str#Dany generate ascii code
+
+
+"""
+
+from i_o.io import output
+
+def run_game() -> None:
+    """Start the game."""
+    print("running")
+
+
+def show_start_screen() -> None:
+    
