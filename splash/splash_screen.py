919925ae5ba21905e0574276e961d9091033da33
Tobbyte <7722596+Tobbyte@users.noreply.github.com>
2026-07-21 07:14:35 +0200

    clear lines

--- splash/splash_screen.py
+++ splash/splash_screen.py
@@ -1,11 +1,17 @@
 """Show a welcome splash_screen."""
 
+from i_o.io import clear_lines, output
 from splash.ascii import SPLASH_MSG, WELCOME_MSG
 
 
 def show_splashscreen() -> None:
     """Draw an ascii art welcome splash."""
-    print(SPLASH_MSG)
+    output(SPLASH_MSG)
+    lines = SPLASH_MSG.count("\n")
+    welcome_msg = WELCOME_MSG + "\n"
+    lines += welcome_msg.count("\n")
     _ = input(
-        WELCOME_MSG + "\n",
+        welcome_msg,
     )
+    lines += 1
+    clear_lines(lines)
