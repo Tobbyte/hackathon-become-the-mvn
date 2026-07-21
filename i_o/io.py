3876a79679932cff77cd027ad63358a4557d7d92
Tobbyte <7722596+Tobbyte@users.noreply.github.com>
2026-07-21 07:43:16 +0200

    remove old  show_menu

--- i_o/io.py
+++ i_o/io.py
@@ -15,12 +15,6 @@ def get_user_input(prompt: str) -> str:
     return input(prompt)
 
 
-def show_menu() -> int:
-    """Present the menu to the user."""
-    output("Here will be menu\n     1: dummy return '1'\n     2: option2")
-    return 1
-
-
 def clear_lines(num_lines: int) -> None:
     """Delete last num_lines lines from output."""
     for _ in range(num_lines):
